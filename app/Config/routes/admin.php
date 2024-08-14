bf3ebdc772522df6e84ecd4d267b1a4bda526bd8
dainq <97323194+dainqd@users.noreply.github.com>
2024-08-14 13:37:25 +0700

    Done admin news.

--- app/Config/routes/admin.php
+++ app/Config/routes/admin.php
@@ -11,9 +11,9 @@ $routes->get('dashboard', 'Admin\AdminHomeController::index', ['as' => 'home.adm
 $routes->group("news", static function ($routes) {
     $routes->get('list', 'Admin\AdminNewsController::list', ['as' => 'admin.news.list']);
     $routes->get('create', 'Admin\AdminNewsController::create', ['as' => 'admin.news.create']);
-    $routes->get('detail(:num)', 'Admin\AdminNewsController::detail/$1', ['as' => 'admin.news.detail']);
+    $routes->get('detail/(:any)', 'Admin\AdminNewsController::detail/$1', ['as' => 'admin.news.detail']);
     $routes->post('create', 'Admin\AdminNewsController::handleCreate', ['as' => 'admin.news.handleCreate']);
-    $routes->put('update(:num)', 'Admin\AdminNewsController::update/$1', ['as' => 'admin.news.update']);
-    $routes->delete('delete/(:num)', 'Admin\AdminNewsController::delete/$1', ['as' => 'admin.news.delete']);
+    $routes->put('update/(:any)', 'Admin\AdminNewsController::update/$1', ['as' => 'admin.news.update']);
+    $routes->delete('delete/(:any)', 'Admin\AdminNewsController::delete/$1', ['as' => 'admin.news.delete']);
 });
 
--- app/Views/admin/news/list.php
+++ app/Views/admin/news/list.php
@@ -18,31 +18,69 @@
             <thead>
             <tr>
                 <th scope="col">#</th>
-                <th scope="col">First</th>
-                <th scope="col">Last</th>
-                <th scope="col">Handle</th>
+                <th scope="col">Tiêu đề</th>
+                <th scope="col">Loại danh mục</th>
+                <th scope="col">Đang hiển thị</th>
+                <th scope="col">Trạng thái</th>
+                <th scope="col">Hành động</th>
             </tr>
             </thead>
             <tbody>
-            <tr>
-                <th scope="row">1</th>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
-            </tr>
-            <tr>
-                <th scope="row">2</th>
-                <td>Jacob</td>
-                <td>Thornton</td>
-                <td>@fat</td>
-            </tr>
-            <tr>
-                <th scope="row">3</th>
-                <td>Larry</td>
-                <td>the Bird</td>
-                <td>@twitter</td>
-            </tr>
+            <?php foreach ($news as $item):
+                $i = 1;
+                ?>
+                <tr>
+                    <th scope="row"><?= $i++ ?></th>
+                    <td><?= $item['title'] ?></td>
+                    <td><?= $item['type'] == 0 ? 'Tin khuyến mãi' : 'Kiến thức ôtô' ?></td>
+                    <td><?= $item['is_show'] == 1 ? 'Có' : 'Không' ?></td>
+                    <td><?= $item['status'] == 1 ? '<span class="text-success">Đang hoạt động </span>' : '<span class="text-danger"> Đã xoá </span>' ?></td>
+                    <td>
+                        <a href="<?= route_to('admin.news.detail', $item['id']) ?> " class="btn btn-primary">
+                            <i class="bi bi-eye"></i>
+                        </a>
+                        <button type="button" data-id="<?= $item['id'] ?>" onclick="confirmDelete('<?= $item['id'] ?>')"
+                                class="btn btnDelete btn-danger"><i class="bi bi-trash"></i></button>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
+
             </tbody>
         </table>
     </section>
+    <script>
+        function confirmDelete(id) {
+            if (confirm('Bạn bán muốn xóa tin này?')) {
+                deleteNews(id);
+            }
+        }
+
+        async function deleteNews(id) {
+            let api = '<?php echo route_to('admin.news.delete', ':id'); ?>';
+            api = api.replace(':id', id);
+
+            try {
+                let result = await fetch(api, {
+                    method: 'DELETE',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Accept': 'application/json'
+                    },
+                });
+
+                if (result.ok) {
+                    alert('Xoá thành công thành công!')
+                    let res = await result.json();
+                    console.log(res);
+                    window.location.reload();
+                } else {
+                    let res = await result.json();
+                    alert(res.message)
+                }
+            } catch (error) {
+                console.error('Error:', error);
+                alert('Đã xảy ra lỗi trong quá trình xoá.');
+            }
+        }
+    </script>
 <?= $this->endSection() ?>
