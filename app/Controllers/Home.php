ebac6310a2eaf3c0c9c155510e372c4433f6c48a
devcuong <devcuongbui1@gmail.com>
2024-08-21 17:10:52 +0700

    [ADD] fetch product

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -8,18 +8,40 @@ class Home extends BaseController
     public function __construct()
     {
         $this->category = model("Category");
+        $this->productModel = model("ProductModel");
     }
 
     public function index()
     {
         $s_parent_code_no = $this->request->getGet('s_parent_code_no') ?: 0;
+
+
         $categories = $this->category->getCategoriesWithSubcategories($s_parent_code_no);
+        $top_categories = $this->category->getTopCategories(5);
+        $topCategoryIds = array_column($top_categories, 'c_idx');
+        $products = $this->productModel->getProductsByCategories($topCategoryIds);
+
+
+        $productsByCategory = [];
+        foreach ($categories as $category) {
+            $categorySlug = $category['slug'];
+            $productsByCategory[$categorySlug] = [];
+
+            foreach ($products as $product) {
+                if ($product['category_id'] == $category['c_idx']) {
+                    $productsByCategory[$categorySlug][] = $product;
+                }
+            }
+        }
+
 
         $data["categories"] = $categories;
         $data["total"] = count($categories);
+        $data["productsByCategory"] = $productsByCategory;
         $data["s_parent_code_no"] = $s_parent_code_no;
 
         return view('index', $data);
     }
+
 
 }
