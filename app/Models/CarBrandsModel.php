46dc55be37710f54a9e3f2c3fdfb52b8f5542c71
devcuong <devcuongbui1@gmail.com>
2024-08-26 17:55:02 +0700

    [ADD] CarBrands

---
+++ app/Models/CarBrandsModel.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class CarBrandsModel extends Model
+{
+    protected $table = 'car_brands';
+    protected $primaryKey = 'c_idx';
+
+    protected $useAutoIncrement = true;
+    protected $allowedFields = [
+        'code_no',
+        'code_name',
+        'parent_code_no',
+        'depth',
+        'status',
+        'contents',
+        'onum',
+        'ufile1',
+        'rfile1',
+        'slug'
+    ];
+
+    public function getCategoriesWithSubcategories($parentCodeNo = 0)
+    {
+        $builder = $this->builder();
+        $builder->select('*');
+        $builder->orderBy("onum", "desc")->orderBy('c_idx', 'desc');
+        $allCategories = $builder->get()->getResultArray();
+
+        $categories = array_filter($allCategories, function($category) use ($parentCodeNo) {
+            return $category['parent_code_no'] == $parentCodeNo;
+        });
+
+        foreach($categories as &$category) {
+            $category['subcategories'] = array_filter($allCategories, function($subcategory) use ($category) {
+                return $subcategory['parent_code_no'] == $category['code_no'];
+            });
+        }
+
+        return $categories;
+    }
+
+    public function getTopCategories($limit = 5)
+    {
+        return $this->orderBy('onum', 'desc')
+            ->orderBy('c_idx', 'desc')
+            ->where('parent_code_no', 0)
+            ->where('onum !=', 0)
+            ->limit($limit)
+            ->findAll();
+    }
+
+}
